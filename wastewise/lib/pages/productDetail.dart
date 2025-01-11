d46d15e80676933d8822b9fac868c2c4eae29c51
haiqalhasly <haiqalhasly@gmail.com>
2025-01-11 08:39:35 +0800

    adding container box around image

--- wastewise/lib/pages/productDetail.dart
+++ wastewise/lib/pages/productDetail.dart
@@ -6,39 +6,59 @@ class Productdetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(80),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'lib/assets/grocerybag.png',
-              height: 300,
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: false,
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.black),
+        backgroundColor: const Color.fromARGB(255, 239, 239, 239),
+        title: const Center(
+          child: Text(
+            "Details",
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 239, 239, 239),
+              // boxShadow: [BoxShadow(blurRadius: 1)]
             ),
-            const Text(
-              'Grocery',
-              style: TextStyle(color: Colors.white, fontSize: 30),
+            width: MediaQuery.of(context).size.width,
+            height: 300,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Image.asset(
+                'lib/assets/grocerybag.png',
+              ),
             ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              const Text(
-                'RM 6.19',
-                style: TextStyle(decoration: TextDecoration.lineThrough),
-              ),
-              SizedBox(
-                width: 5,
-              ),
-              const Text(
-                'RM 6.19',
-                style: TextStyle(
-                    fontSize: 28,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-            ]),
-            ElevatedButton(
+          ),
+          const Text(
+            'Grocery',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
+          const Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Text(
+              'RM 6.19',
+              style: TextStyle(decoration: TextDecoration.lineThrough),
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              'RM 6.19',
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+          ]),
+          Center(
+            child: ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
+                  backgroundColor: const Color(0xFF396241),
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20)),
                   minimumSize: const Size(350, 60), // Width 200 and height 60
@@ -47,10 +67,10 @@ class Productdetail extends StatelessWidget {
                 ),
                 child: const Text(
                   'Add to cart',
-                  style: TextStyle(fontSize: 30),
-                ))
-          ],
-        ),
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )),
+          )
+        ],
       ),
     );
   }
